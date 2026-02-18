46b3abc2022554d8966139fe5da5234b0552b750
tavogus007 <gustavog095065@gmail.com>
2026-02-18 16:22:59 -0400

    Feat: :art: Prod and services component added, base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent }  from './core/navbar/navbar.component';
 import { HomeComponent }  from './features/home/home.component';
 import { CoreValuesComponent }  from './body/core-values/core-values.component';
 import { FocusAreasComponent } from "./body/focus-areas/focus-areas.component";
+import { ProdAndServComponent } from "./body/prod-and-serv/prod-and-serv.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, CoreValuesComponent, FocusAreasComponent],
+  imports: [NavbarComponent, HomeComponent, CoreValuesComponent, FocusAreasComponent, ProdAndServComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/body/prod-and-serv/prod-and-serv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prod-and-serv',
+  imports: [],
+  templateUrl: './prod-and-serv.component.html',
+  styleUrl: './prod-and-serv.component.css'
+})
+export class ProdAndServComponent {
+
+}
