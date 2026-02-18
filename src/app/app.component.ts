07702ef09715d891e05fbd340575db37105cd13f
tavogus007 <gustavog095065@gmail.com>
2026-02-18 17:15:26 -0400

    Feat: :art: Footer section attemp1 and news section, base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { HomeComponent }  from './features/home/home.component';
 import { CoreValuesComponent }  from './body/core-values/core-values.component';
 import { FocusAreasComponent } from "./body/focus-areas/focus-areas.component";
 import { ProdAndServComponent } from "./body/prod-and-serv/prod-and-serv.component";
+import { NewsComponent } from "./body/news/news.component";
+import { FooterComponent } from "./body/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HomeComponent, CoreValuesComponent, FocusAreasComponent, ProdAndServComponent],
+  imports: [NavbarComponent, HomeComponent, CoreValuesComponent, FocusAreasComponent, ProdAndServComponent , NewsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
